85fb5fd58b6ed753e63bb5ff95c130199e9ed7f4
HECHONG999 <1259796851@qq.com>
2020-05-07 16:47:00 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React, { useState,useRef , useLayoutEffect} from 'react';
-import {getAllStudent} from "./services/getAllStudent"
+import React, { useState, } from 'react';
+import { HashRouter, BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 
+// function A() {
+//   return <h1>组件A</h1>
+// }
+
+// function B() {
+//   return <h1>组件B</h1>
+// }
 
 
-function App() { 
-  const [n, setN] = useState(10)
-  const testRef = useRef();
-  const ulRef = useRef()
-  useLayoutEffect(() => {
+// function App() {
 
-    testRef.current.innerText = Math.random().toFixed(3)
-  })
-  return (
-     <div>
-       <h1 ref={testRef}>{n}</h1>
-        <ul ref={ulRef}></ul>
-       <button onClick={ () => {
-         setN(1000)
-       }}>改变文本</button>
-     </div>
-    )
+//   return (
+//     /**
+//      * 属性: 
+//      * 1、exact 组件进行精确匹配,只有path路径绝对相等时才会进行匹配展示
+//      * 组件:
+//      * 1、Switch：严格匹配一旦匹配到一个组件，后续的组件将不在进行匹配
+//      * 
+//      * Route组件内存在一个函数,并且返回一个React对象, UI渲染一定会进行渲染展示到页面, 一旦成功渲染,其他路由组件将不会再进行匹配渲染
+//      */
+//     <Router>
+//       <Switch>
+//         <Route path="/a" component={A}></Route>
+//         <Route path="/a" component={B}></Route>
+//       </Switch>
+//     </Router>
+//   )
+// }
+
+// export default App;
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleString()}</h2>
+      </div>
+    );
+  }
 }
-
-export default App;
