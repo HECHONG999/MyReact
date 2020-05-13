d50bb073fef5aa957095f0fc3c3350f64daa6d64
HECHONG999 <1259796851@qq.com>
2020-05-13 20:50:41 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React, { useState } from 'react'
-import { CSSTransition } from "react-transition-group";
+import { CSSTransition,SwitchTransition } from "react-transition-group";
 import "./App.css";
 import "animate.css";
-let duration = 800;
+let duration = 3000;
 
-function MyTransition(props) {
-    console.log(props)
-    return <CSSTransition 
-                    appear mountOnEnter 
-                    timeout={duration} 
-                    in={props.visible}
-                    classNames={{  // animate使用: 在需要进行动画的元素上设置animate-animated类名
-                        exitActive: "animate__bounceOutLeft animate__animated",
-                        exitDone: "exit-done",
-                        enterActive: "animate__backInRight animate__animated",
-                        appearActive: "animate__backInRight animate__animated"
-                    }}
-                    >
-        {props.children}
-    </CSSTransition>
-}
-function Comp1() {
-    return <div className="title animate__fast">
-        <h1>Comp1</h1>
-        <div>我是一个标题</div>
-    </div>
-}
-
-function Comp2() {
-     return <div className="titleanimate__fast">
-        <h1>Comp2</h1>
-        <div>我是一个标题</div>
-    </div>
-
-}
 export default function App() {
 
     let [inProp, setInProp] = useState(false);
     return (
         <div className="container">
-            <div className="com-wrapper">
-              <MyTransition visible={inProp}>
-                  <Comp1/>
-              </MyTransition>
-              <MyTransition visible={!inProp}>
-                  <Comp2/>
-              </MyTransition>
-            </div>
+            <SwitchTransition mode="in-out" >
+                    <CSSTransition key={inProp} appear in={inProp} timeout={duration} 
+                                    classNames={{
+                                        enter: "animate__animated bounceIn ",
+                                        exit: "animate__animated bounceOut",
+                                    }}
+                        >
+                            <h1 className="animated fast">{inProp ? "title1" : "title2"}</h1>
+                    </CSSTransition>
+                    
+            </SwitchTransition>
             <button onClick={() => {
                 setInProp(!inProp)
             }}> Click to Toggle </button>
         </div>
     )
 }
+
