19864e689c3f4903752560dc50f62815cd29c8ba
HECHONG999 <1259796851@qq.com>
2020-05-13 21:18:22 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { useState } from 'react'
-import { CSSTransition,SwitchTransition } from "react-transition-group";
+import { CSSTransition, SwitchTransition, TransitionGroup } from "react-transition-group";
 import "./App.css";
 import "animate.css";
+import { v4 as uuidv4 } from 'uuid';
 let duration = 3000;
 
 export default function App() {
 
-    let [inProp, setInProp] = useState(false);
+    let [taskList, setTasklist] = useState([
+        { id: uuidv4(), task: "任务一" },
+        { id: uuidv4(), task: "任务二" }
+    ]);
+
     return (
         <div className="container">
-            <SwitchTransition mode="in-out" >
-                    <CSSTransition key={inProp} appear in={inProp} timeout={duration} 
-                                    classNames={{
-                                        enter: "animate__animated bounceIn ",
-                                        exit: "animate__animated bounceOut",
-                                    }}
-                        >
-                            <h1 className="animated fast">{inProp ? "title1" : "title2"}</h1>
-                    </CSSTransition>
-                    
-            </SwitchTransition>
-            <button onClick={() => {
-                setInProp(!inProp)
-            }}> Click to Toggle </button>
+            <TransitionGroup>
+                {
+                    taskList.map(item => (
+
+                        <CSSTransition  key={item.id} timeout={1000}>
+                            <div> 
+                                <div>
+                                <div>{item.task}</div>
+                                <button onClick={ () => {
+                                    setTasklist(taskList.filter( it => it.id !== item.id))
+                                }}>删除</button>
+                                </div>
+                                <div>
+                                    <button onClick={ () => {
+                                       var  name =  window.prompt("请添加一个学生")
+                                       console.log(name)
+                                       if(name == null) { return }
+                                       setTasklist([...taskList, {task: name, id: uuidv4()}])
+                                    }}>添加Task</button>
+                                </div>
+                            
+                            </div>
+                        </CSSTransition>
+
+                      
+                    ))
+                }
+            </TransitionGroup>
         </div>
     )
 }
