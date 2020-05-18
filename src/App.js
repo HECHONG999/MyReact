759dd9a74e28a98cf905b084a4d0e7449457b680
HECHONG999 <1259796851@qq.com>
2020-05-18 13:31:15 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React, { useState } from 'react'
-import { CSSTransition, SwitchTransition, TransitionGroup } from "react-transition-group";
-import "animate.css";
-import { v4 as uuidv4 } from 'uuid';
-let duration = 3000;
-
+import "./Redux/index"
 export default function App() {
-
-    let [taskList, setTasklist] = useState([
-        { id: uuidv4(), task: "任务一" },
-        { id: uuidv4(), task: "任务二" }
-    ]);
-
     return (
-        <div className="container">
-            <TransitionGroup>
-                {
-                    taskList.map(item => (
-                        <CSSTransition key={item.id} timeout={1000}>
-                            <div>
-                                <div>
-                                    <div>{item.task}</div>
-                                    <button onClick={() => {
-                                        setTasklist(taskList.filter(it => it.id !== item.id))
-                                    }}>删除</button>
-                                </div>
-                            </div>
-                        </CSSTransition>
-                    ))
-                }
-            </TransitionGroup>
-            <div>
-                <button onClick={() => {
-                    var name = window.prompt("请添加一个学生")
-                    console.log(name)
-                    if (name == null) { return }
-                    setTasklist(item => [
-                        ...item,
-                        { task: name, id: uuidv4() }
-                    ])
-                }}>添加Task</button>
-            </div>
+        <div >
+           React !!!!!
         </div>
     )
 }
