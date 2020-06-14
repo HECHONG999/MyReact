0d4e8276a74ac9b89dc88d5b83053c1faa6d224b
HECHONG999 <1259796851@qq.com>
2020-06-15 00:44:38 +0800

    commit

--- src/Store/Reducer/loginUser.js
+++ src/Store/Reducer/loginUser.js
@@ -1,6 +1,6 @@
 import {SETLOGINUSERTYPE} from "../action/loginUserAction";
 
-const initialState = null
+const initialState = [{name: "我是何冲", id: "hlillidhytligh1231"}]
 // action的reducer函数
 export default function (state = initialState, {type, payload}) {
     switch(type) {
--- src/Store/Reducer/users.js
+++ src/Store/Reducer/users.js
@@ -20,3 +20,6 @@ export default (state = initialState, {type, payload}) => {
             return state
     }
 } 
+
+
+
