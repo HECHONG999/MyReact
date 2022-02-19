d1ecf4fffc97817baa476a85ede33f2f532c5763
HECHONG999 <1259796851@qq.com>
2022-02-19 10:19:42 +0800

    matchPath

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React ,{Component} from 'react'
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import RouterGuard from "./RouterGuard"
-
+import matchPath from './react-router/matchPath';
 
 function PageA() {
     return (
@@ -21,16 +20,7 @@ function PageB() {
 export default function App() {
     return (
 
-        <RouterGuard onBeforeChange={ (prev, cur, action, commit, unBlock) => {  // commitCallBack(true) 表示可以通过路由守备
-            // 判断的逻辑
-            //该函数做判断是否守备路由
-            commit(true);
-            unBlock()
-
-
-        }} onChange={ (preLocation, location, action) => {
-            console.log(`日志: 从组件页面${preLocation.pathname} --->> 到组件页面 ${location.pathname} 跳转方式${action}`)
-        }}>
+        <Router>
             <nav>
                 <Link to="/pageA">PageA</Link>
                 <Link to="/pageB">PageB</Link>
@@ -40,6 +30,6 @@ export default function App() {
                 <Route path="/pageA" component={PageA} />
                 <Route path="/pageB" component={PageB} />
             </div>
-        </RouterGuard>
+        </Router>
     )
 }
---
+++ src/react-router/matchPath.js
@@ -0,0 +1,65 @@
+import {pathToRegexp}  from "path-to-regexp";
+
+/**
+ * 得到匹配结果 （match对象）， 没有匹配返回null
+ * @param {*} path 
+ * @param {*} options 相关配置，该配置是一个对象，该对象中，可以出现：exact、sensitive、strict
+ */
+export default function matchPath(path, options) {
+    const pathname = window.location.pathname;
+    let keys = [];
+    const regExp = pathToRegexp(path, keys, getOptions(options));
+    const result = regExp.exec(pathname);
+    console.log(result)
+    if(!result) {
+        return null;
+    }
+
+    // 如果匹配
+    let groups = Array.from(result);
+    groups = groups.splice(1);      // 得到匹配的分组结果
+    let params = getParams(groups, keys);
+    return {
+        isExact: pathname == result[0],
+        params,
+        path,
+        url: result[0]
+    }
+}
+
+/**
+ * 根据匹配的分组结果， 得到一个params 对象
+ * @param {*} groups 
+ * @param {*} keys 
+ * @returns 
+ */
+function getParams(groups, keys) {
+    let obj = {};
+    for(let i = 0; i < groups.length; i ++) {
+        let value = groups[i];
+        let name = keys[i].name;
+        obj[name] = value
+    }
+    return obj;
+}
+
+/**
+ * 将react-router的配置，转为 path-to-regexp的配置
+ * @param {*} options 
+ * @returns 
+ */
+function getOptions(options={}) {
+    let defaultOptions = {
+        exact: false,
+        sensitive: false,
+        strict: false
+    }
+    let opts = {...defaultOptions, ...options}
+    return {
+        sensitive: opts.sensitive,
+        exact: opts.exact,
+        strict: opts.strict
+    }
+}
+
+console.log(matchPath("/news/:name/:age?", {}))
